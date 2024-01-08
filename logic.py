ba8f4d999071fbea67a301d7512dd8e8a6d56a9f
elsebaiy elsebaiy192156 <elsebaiy192156@fci.bu.edu.eg>
2024-01-08 18:05:35 +0200

    remove ald files

--- logic.py
+++ logic.py
@@ -1,94 +1,94 @@
-from g2p_en import G2p
-from string import punctuation 
-from scoring import calculate_fluency_and_pronunciation
+# # from g2p_en import G2p
+# from string import punctuation 
+# from scoring import calculate_fluency_and_pronunciation
 
-import whisper 
-import torch 
+# import whisper 
+# import torch 
 
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+# device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-model = whisper.load_model("base.en", device=device)
+# model = whisper.load_model("base.en", device=device)
 
-def transcribe(audio):
-    result = model.transcribe(audio, word_timestamps=False, no_speech_threshold=0.4,  compression_ratio_threshold=2, temperature=0)
-    return {'language': result['language'], 'text': result['text']}
+# def transcribe(audio):
+#     result = model.transcribe(audio, word_timestamps=False, no_speech_threshold=0.4,  compression_ratio_threshold=2, temperature=0)
+#     return {'language': result['language'], 'text': result['text']}
 
-def remove_punctuation(text):
-    return ''.join(list(map(lambda x: '' if x in punctuation else x, text)))
-def text2phoneme(text):
-    g2p = G2p()
-    text = text.lower()
-    text = remove_punctuation(text)
-    phonemes = g2p(text)
-    phonemes.append(' ')
-    new_phones = list()
-    word = []
-    for ph in phonemes:
-        if ph == ' ':
-            new_phones.append(word)
-            word = []
-        else:
-            word.append(ph)
-    return new_phones
-
-
-def rate_pronunciation(expected_phonemes, actual_phonemes):
-    expected_phonemes = expected_phonemes
-    actual_phonemes = actual_phonemes
-    # Calculate the Levenshtein distance between the two phoneme sequences
-    results = []
-    for i, base_word in enumerate(actual_phonemes): # ['ss', 'ad', ss]
-        best_dist = float('inf')
-        if i <= len(expected_phonemes): 
-            word_best_score = float('inf')
-            for window_idx in range(i, i + min(2, len(expected_phonemes) - i)):
-                expected_word = expected_phonemes[window_idx]
-                missed = 0
-                for b_ph, ex_ph in zip(base_word, expected_word):
-                    if b_ph != ex_ph:
-                        missed += 1
-                len_error = abs(len(base_word) - len(expected_word))
-                missed = missed * 0.5
-                missed += len_error
-                missed = missed / len(base_word)
-                if missed < word_best_score:
-                    word_best_score = missed
-                if -0.000001 < word_best_score < 0.000001:
-                    word_best_score = 0
-                    break
-
-        if word_best_score == 0:
-           results.append(3) 
-        elif word_best_score <= 0.40:
-            results.append(2) 
-        else:
-            results.append(1) 
-    return results
+# def remove_punctuation(text):
+#     return ''.join(list(map(lambda x: '' if x in punctuation else x, text)))
+# def text2phoneme(text):
+#     g2p = G2p()
+#     text = text.lower()
+#     text = remove_punctuation(text)
+#     phonemes = g2p(text)
+#     phonemes.append(' ')
+#     new_phones = list()
+#     word = []
+#     for ph in phonemes:
+#         if ph == ' ':
+#             new_phones.append(word)
+#             word = []
+#         else:
+#             word.append(ph)
+#     return new_phones
 
 
+# def rate_pronunciation(expected_phonemes, actual_phonemes):
+#     expected_phonemes = expected_phonemes
+#     actual_phonemes = actual_phonemes
+#     # Calculate the Levenshtein distance between the two phoneme sequences
+#     results = []
+#     for i, base_word in enumerate(actual_phonemes): # ['ss', 'ad', ss]
+#         best_dist = float('inf')
+#         if i <= len(expected_phonemes): 
+#             word_best_score = float('inf')
+#             for window_idx in range(i, i + min(2, len(expected_phonemes) - i)):
+#                 expected_word = expected_phonemes[window_idx]
+#                 missed = 0
+#                 for b_ph, ex_ph in zip(base_word, expected_word):
+#                     if b_ph != ex_ph:
+#                         missed += 1
+#                 len_error = abs(len(base_word) - len(expected_word))
+#                 missed = missed * 0.5
+#                 missed += len_error
+#                 missed = missed / len(base_word)
+#                 if missed < word_best_score:
+#                     word_best_score = missed
+#                 if -0.000001 < word_best_score < 0.000001:
+#                     word_best_score = 0
+#                     break
+
+#         if word_best_score == 0:
+#            results.append(3) 
+#         elif word_best_score <= 0.40:
+#             results.append(2) 
+#         else:
+#             results.append(1) 
+#     return results
 
 
-def Speaker_speech_analysis(audio_path, text):
-    pre_transcribtion = transcribe(audio_path)['text']
-    print(pre_transcribtion)
-    transcribtion = text2phoneme(pre_transcribtion)
-    text_phone    = text2phoneme(text)
-    scores        = rate_pronunciation(transcribtion, text_phone)
-    FP_scores     = calculate_fluency_and_pronunciation(audio_path, len(pre_transcribtion.split()), scores, len(text.split()))
-    word_scores = [(word, s) for word, s in zip(text.split(), scores)]
+
+
+# def Speaker_speech_analysis(audio_path, text):
+#     pre_transcribtion = transcribe(audio_path)['text']
+#     print(pre_transcribtion)
+#     transcribtion = text2phoneme(pre_transcribtion)
+#     text_phone    = text2phoneme(text)
+#     scores        = rate_pronunciation(transcribtion, text_phone)
+#     FP_scores     = calculate_fluency_and_pronunciation(audio_path, len(pre_transcribtion.split()), scores, len(text.split()))
+#     word_scores = [(word, s) for word, s in zip(text.split(), scores)]
     
-    FP_scores['word_scores'] = word_scores
-    return FP_scores
+#     FP_scores['word_scores'] = word_scores
+#     return FP_scores
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    text = 'i have ADHD '
-    text = text2phoneme(text)
-    file_path = r'user_recording.wav'
-    trans = transcribe(file_path)['text']
-    print(trans)
-    trans = text2phoneme(trans)
-    print('base:', text)
-    print('predicted:', trans)
-    result = rate_pronunciation(trans, text)
-    print(result)
+#     text = 'i have ADHD '
+#     text = text2phoneme(text)
+#     file_path = r'user_recording.wav'
+#     trans = transcribe(file_path)['text']
+#     print(trans)
+#     trans = text2phoneme(trans)
+#     print('base:', text)
+#     print('predicted:', trans)
+#     result = rate_pronunciation(trans, text)
+#     print(result)
--- test.py
+++ test.py
@@ -1,18 +1,57 @@
-from allosaurus.app import read_recognizer
+import librosa
+import numpy as np
+import os
+import scipy.stats as stats
 
-# load your model by the <model name>, will use 'latest' if left empty
-model = read_recognizer('latest')
 
-# # run inference on <audio_file> with <lang>, lang will be 'ipa' if left empty
-# y = model.recognize(r"C:\Users\20101\Downloads\LDC93S1.wav", 'ipa', timestamp=False)
-# print(y)
-# print()
+def normalize_aspect(value, mean, std):
+    """ Normalize an aspect of speech using normal distribution. """
+    return stats.norm(mean, std).cdf(value)
 
-# y = model.recognize(r"C:\Users\20101\Downloads\LDC93S1.wav", 'eng' , timestamp=False)
-# print(y)
+def calculate_fluency_scores(audio, total_words, content_score, pron_score):
+    # Constants
+    sample_rate = 16000  # Assuming a sample rate of 16 kHz
+    # Define means and standard deviations for fluent speech
+    speech_rate_mean, speech_rate_std = 150, 40
+    phonation_time_mean, phonation_time_std = 50, 4
 
-import phonemizer
-text = 'Hello'
-ipa = phonemizer.phonemize(text,)
+    # Calculate speaking and total duration
+    non_silent_intervals = librosa.effects.split(audio, top_db=20)
+    speaking_time = sum([intv[1] - intv[0] for intv in non_silent_intervals]) / sample_rate
+    total_duration = len(audio) / sample_rate
 
-print(ipa)
+    
+    # Phonation time ratio
+    phonation_time_ratio = speaking_time / total_duration * 60
+
+    phonation_time_ratio = normalize_aspect(phonation_time_ratio, phonation_time_mean, phonation_time_std)
+    if phonation_time_ratio > 0.5: 
+        phonation_time_ratio =  0.5 - (phonation_time_ratio - 0.5)
+    phonation_time_ratio = (phonation_time_ratio / 0.5) * 1
+    
+    
+    speech_rate = (total_words / total_duration) * 60
+
+    speech_rate_score = normalize_aspect(speech_rate, speech_rate_mean, speech_rate_std)
+    if speech_rate_score > 0.5: 
+        speech_rate_score =  0.5 - (speech_rate_score - 0.5)
+
+    speech_rate_score = (speech_rate_score / 0.5) * 1
+    
+    content_score, pron_score = content_score / 100, pron_score / 100
+
+    w_rate_score = 0.45
+    w_pho_ratio  = 0.30
+    w_cont         = 0.05
+    w_pro           = 0.2
+    scaled_fluency_score = speech_rate_score * w_rate_score + phonation_time_ratio * w_pho_ratio + content_score * w_cont + pron_score * w_pro
+    return scaled_fluency_score
+
+
+a, _ = librosa.load(r"D:\test cases\Test 1\33\33-RA-Reptile.wav", sr=16000)
+text = 'Around 250 million years ago, 700 species of reptiles closely related to the modern-day crocodile roamed the earth, now new research reveals how a complex interplay between climate change, species competition and habitat can help explain why just 23 species of crocodile survive today.'
+
+word_count = len(text.split())
+content = 95
+
+print(calculate_fluency_scores(a, word_count, content))
